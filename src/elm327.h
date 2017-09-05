c1e76997ea55d606afec0ab52dfbb4907c32fdc8
Jeremy Dyer <jeremydyer@apache.org>
2017-09-05 18:18:46 -0400

    read input after atz

--- src/elm327.h
+++ src/elm327.h
@@ -39,6 +39,14 @@ class elm327 {
     try {
       const char* data = "ATZ";
       sp.write_some(boost::asio::buffer(data, strlen(data)));
+
+      std::cout << "Reading the input after ATZ reset" << std::endl;
+      char c;
+      do {
+        sp.read_some(boost::asio::buffer(&c, 1));
+        std::cout << "Read data: '" << c << "' from the serial port";
+      } while(c != '>');
+
     } catch (...) {
       std::cout << "Error occured while trying to reset ATZ" << std::endl;
       exit(-1);
@@ -50,6 +58,24 @@ class elm327 {
       sp.write_some(boost::asio::buffer(atData, strlen(atData)));
     } catch (...) {
       std::cout << "Error setting ATE0 to Echo OFF" << std::endl;
+      exit(-1);
+    }
+
+    // ATH1 Headers on
+    try {
+      const char *athData = "ATH1";
+      sp.write_some(boost::asio::buffer(athData, strlen(athData)));
+    } catch (...) {
+      std::cout << "Error setting ATH1 Header on" << std::endl;
+      exit(-1);
+    }
+
+    // ATL0 Linefeeds Off
+    try {
+      const char *atlData = "ATL0";
+      sp.write_some(boost::asio::buffer(atlData, strlen(atlData)));
+    } catch (...) {
+      std::cout << "Error setting ATL0 Linefeeds Off" << std::endl;
       exit(-1);
     }
 
