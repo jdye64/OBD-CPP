8c053bd6ad640725e37022af19771b2f4125069e
Jeremy Dyer <jeremydyer@apache.org>
2017-09-05 17:21:27 -0400

    ugh update to use the actual correct serial port

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
   std::cout << "Reading vehicle OBD data" << std::endl;
   std::cout << "Creating OBD connection in Main ...." << std::endl;
   obd obd;
-  obd.connect("/dev/rcomm0", 115200);
+  obd.connect("/dev/rfcomm0", 115200);
   std::cout << "Done creating OBD object" << std::endl;
 
   return 0;
