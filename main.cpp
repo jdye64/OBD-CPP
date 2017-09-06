b78dd65aafd607806c8cba03a1228e72bc4aaac0
Jeremy Dyer <jeremydyer@apache.org>
2017-09-05 21:18:55 -0400

    added read method

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
   std::cout << "Reading vehicle OBD data" << std::endl;
   std::cout << "Creating OBD connection in Main ...." << std::endl;
   obd obd;
-  obd.connect("/dev/rfcomm0", 115200);
+  obd.connect("/dev/rfcomm0", 38400);
   std::cout << "Done creating OBD object" << std::endl;
 
   return 0;
--- src/elm327.h
+++ src/elm327.h
@@ -7,6 +7,7 @@
 
 #include "OBDStatus.h"
 #include "iostream"
+#include <sstream>
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
 
@@ -14,109 +15,27 @@ class elm327 {
 
  public:
 
-  explicit elm327(const char* portStr, int baudRate) {
-
-    std::cout << "Open ELM327 interface connection" << std::endl;
-
-    // Open connection port
-    std::cout << "Opening serial port now" << std::endl;
-    static boost::asio::io_service ios;
-    boost::asio::serial_port sp(ios, portStr);
-    std::cout << "Is /dev/rfcomm0 open?: " << sp.is_open() << std::endl;
-
-
-    //Set ASIO options
-    std::cout << "Setting boost.asio options on serial connection" << std::endl;
-    sp.set_option(boost::asio::serial_port::baud_rate(baudRate));
-
-//    char c;
-//
-//    boost::asio::read(sp, boost::asio::buffer(&c,1));
-//    sp.close();
-//    std::cout << "Read character: '" << c << "' from the serial port" << std::endl;
-
-    // ATZ (reset)
-    try {
-      const char* data = "ATZ";
-      sp.write_some(boost::asio::buffer(data, strlen(data)));
-
-      std::cout << "Reading the input after ATZ reset" << std::endl;
-      char c;
-      do {
-        sp.read_some(boost::asio::buffer(&c, 1));
-        std::cout << "Read data: '" << c << "' from the serial port" << std::endl;
-      } while(c != '>');
-
-    } catch (...) {
-      std::cout << "Error occured while trying to reset ATZ" << std::endl;
-      exit(-1);
-    }
-
-    // ATE0 Echo Off
-    try {
-      const char *atData = "ATE0";
-      sp.write_some(boost::asio::buffer(atData, strlen(atData)));
-
-      std::cout << "Reading the input after ATE0 Echo Off" << std::endl;
-      char c;
-      do {
-        sp.read_some(boost::asio::buffer(&c, 1));
-        std::cout << "Read data: '" << c << "' from the serial port" << std::endl;
-      } while(c != '>');
-
-    } catch (...) {
-      std::cout << "Error setting ATE0 to Echo OFF" << std::endl;
-      exit(-1);
-    }
-
-    // ATH1 Headers on
-    try {
-      const char *athData = "ATH1";
-      sp.write_some(boost::asio::buffer(athData, strlen(athData)));
-
-      std::cout << "Reading the input after ATH1 Headers On" << std::endl;
-      char c;
-      do {
-        sp.read_some(boost::asio::buffer(&c, 1));
-        std::cout << "Read data: '" << c << "' from the serial port" << std::endl;
-      } while(c != '>');
-
-    } catch (...) {
-      std::cout << "Error setting ATH1 Header on" << std::endl;
-      exit(-1);
-    }
-
-    // ATL0 Linefeeds Off
-    try {
-      const char *atlData = "ATL0";
-      sp.write_some(boost::asio::buffer(atlData, strlen(atlData)));
-
-      std::cout << "Reading the input after ATL0 Linefeeds Off" << std::endl;
-      char c;
-      do {
-        sp.read_some(boost::asio::buffer(&c, 1));
-        std::cout << "Read data: '" << c << "' from the serial port" << std::endl;
-      } while(c != '>');
-
-    } catch (...) {
-      std::cout << "Error setting ATL0 Linefeeds Off" << std::endl;
-      exit(-1);
-    }
-
-    std::cout << std::endl << "Finished connecting to ELM327 Interface!!!" << std::endl;
-  }
+  elm327(const char* portStr, int baudRate);
 
   ~ elm327() {
-    //sp.close();
+    std::cout << "Closing ELM327 Interface" << std::endl;
+    _serialPort.close();
   }
 
  protected:
 
   static const char _ELM_PROMPT = '>';
 
+  std::string _send(const char* cmd, int msDelay);
 
+  std::string _write(const char* cmd, intmsDelay);
+
+  std::string _read();
 
  private:
+
+  boost::asio::serial_port _serialPort;
+  int _defaultReadBytes = 64;
 
 };
 
